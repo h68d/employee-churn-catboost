8b96b03cc49196e0cc6e1e7703151ca570060e51
HD <125695399+h68d@users.noreply.github.com>
2023-09-14 22:11:02 +0200

    Update my_app3.py

--- my_app3.py
+++ my_app3.py
@@ -12,11 +12,10 @@ de_05_chur_transformer = pickle.load(open('column_trans_model', "rb"))
 de_05_chur_scaler = pickle.load(open("scaler_model", "rb"))
 de_05_chur_model = pickle.load(open("catboost_model", "rb"))
 
-
+last_evaluation = st.sidebar.slider("What is your last evaluation:", min_value=0, max_value=1, value=10, step=1)
 department= st.selectbox("Select your work department", ('sales','technical','support','IT','RandD','product_mng','marketing','accounting','hr','management'))
 salary = st.selectbox("Select your salary level", ('low', 'medium', 'high'))
 satisfaction_level = st.sidebar.number_input("What is your satisfaction level:",  min_value=0, max_value=10)
-last_evaluation = st.sidebar.slider("What is your last evaluation:", min_value=0, max_value=1, value=10, step=1)
 number_project = st.sidebar.selectbox("What is your number of project:", (2,3,4,5,6,7))
 average_montly_hours = st.sidebar.slider("What is your average montly hours?", min_value=96, max_value=310, value=150,step=7)
 time_spend_company = st.sidebar.slider("What is your time spend company:", min_value=2, max_value=10, value=5,step= 1)
