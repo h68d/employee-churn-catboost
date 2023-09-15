59739a1ce9eaf442a66eaf7fd3a659766b28f546
HD <125695399+h68d@users.noreply.github.com>
2023-09-15 04:25:49 +0200

    Update my_app4.py

--- my_app4.py
+++ my_app4.py
@@ -2,8 +2,8 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-st.title("Welcome to our free employer behaviour prediction site")
-st.text("Please ask your eployee following questions. Depending of their answer our model  \nwill predic whether they stay or leave. You can use left-sidebar to specify extra \nspecifications. Please enjoy using our model. ")
+st.title("Welcome to our employee commitment prediction site")
+st.text("Please ask your eployee following questions. Depending of their answer our model  \nwill predict whether they stay or leave. You can use left-sidebar to specify extra \nspecifications. Please enjoy using our model. ")
 
 # To load machine learning model
 import pickle
@@ -13,27 +13,27 @@ de_05_chur_scaler = pickle.load(open("scaler_model", "rb"))
 de_05_chur_model = pickle.load(open("catboost_model", "rb"))
 
 
-department= st.selectbox("Select your work department", ('sales','technical','support','IT','RandD','product_mng','marketing','accounting','hr','management'))
-salary = st.selectbox("Select your salary level", ('low', 'medium', 'high'))
-satisfaction_level = st.sidebar.selectbox("What is your satisfaction level:", (0,0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1))
-last_evaluation = st.sidebar.selectbox("What is your last evaluation:", (0.36,0.4,0.5,0.6,0.7,0.8,0.9,1))
-number_project = st.sidebar.selectbox("What is your number of project:", (2,3,4,5,6,7))
-average_montly_hours = st.sidebar.slider("What is your average montly hours?", min_value=96, max_value=310, value=150,step=7)
-time_spend_company = st.sidebar.slider("What is your time spend company:", min_value=2, max_value=10, value=5,step= 1)
-work_accident = st.sidebar.selectbox("What is your work accident:",(0,1))
-promotion_last_5years = st.sidebar.selectbox("What is your promotion last in the 5 years:",(0,1))
+department= st.selectbox("Department of the employee", ('sales','technical','support','IT','RandD','product_mng','marketing','accounting','hr','management'))
+salary = st.selectbox("Employee salary level", ('low', 'medium', 'high'))
+satisfaction_level = st.sidebar.selectbox("Employee satisfaction level:", (0,0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1))
+last_evaluation = st.sidebar.selectbox("Last employee performance evaluation grade:", (0.36,0.4,0.5,0.6,0.7,0.8,0.9,1))
+number_project = st.sidebar.selectbox("Number of project given to the employee:", (2,3,4,5,6,7))
+average_montly_hours = st.sidebar.slider("Average montly working hours of the employee", min_value=96, max_value=310, value=150,step=7)
+time_spend_company = st.sidebar.slider("Total time employee spent in the organization:", min_value=2, max_value=10, value=5,step= 1)
+work_accident = st.sidebar.selectbox("Did the employee had any work accidents?:",(0,1))
+promotion_last_5years = st.sidebar.selectbox("Did the employee had promotion in the last 5 years?:",(0,1))
 
 
 my_dict = {
-    "satisfaction_level": satisfaction_level,
-    "last_evaluation": last_evaluation,
-    "number_project": number_project,
-    "average_montly_hours": average_montly_hours,
-    'time_spend_company':time_spend_company,
-    "work_accident": work_accident, 
-    'promotion_last_5years':promotion_last_5years,
-    "department": department, 
-    "salary": salary
+    "Employee satisfaction level": Employee satisfaction level,
+    "Last employee performance evaluation grade": Last employee performance evaluation grade,
+    "Number of project given to the employee": Number of project given to the employee,
+    "Average montly working hours of the employee": Average montly working hours of the employee,
+    'Total time employee spent in the organization':Total time employee spent in the organization,
+    "Did the employee had any work accidents?": Did the employee had any work accidents?, 
+    'Did the employee had promotion in the last 5 years?':Did the employee had promotion in the last 5 years?,
+    "Department of the employee": Department of the employee, 
+    "Employee salary level": Employee salary level
     }
 
 my_dict2 = {
